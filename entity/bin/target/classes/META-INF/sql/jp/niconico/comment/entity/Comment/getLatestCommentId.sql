c763415d91b001c87e5ac40fcd1a064a64676665
honda <yhonda1001@gmail.com>
2015-02-23 22:47:13 +0900

    remove bin Dir.

--- entity/bin/target/classes/META-INF/sql/jp/niconico/comment/entity/Comment/getLatestCommentId.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-	max(T1_.COMMENT_ID) as COMMENT_ID
-	
-FROM comment as T1_
-
-WHERE ROOM_ID = /*roomId*/0
-
-;
--- entity/bin/target/classes/META-INF/sql/jp/niconico/comment/entity/S2session/deleteAllExpired.sql
+++
@@ -1,5 +0,0 @@
-DELETE T1_
-
-FROM S2session as T1_
-
-WHERE T1_.LAST_ACCESS < /*expiredDatetime*/1;
