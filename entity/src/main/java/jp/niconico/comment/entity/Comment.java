7592df8242bbd765e1884cdb7e47697cb207c892
honda <yhonda1001@gmail.com>
2015-02-26 00:51:09 +0900

    serialize comment content

--- entity/src/main/java/jp/niconico/comment/entity/Comment.java
+++ entity/src/main/java/jp/niconico/comment/entity/Comment.java
@@ -2,6 +2,7 @@ package jp.niconico.comment.entity;
 
 import java.io.Serializable;
 import java.sql.Timestamp;
+
 import javax.annotation.Generated;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -9,6 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
@@ -21,37 +23,38 @@ import javax.persistence.Table;
 @Table(name = "comment")
 public class Comment implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    /** COMMENT_ID */
-    @Id
-    @GeneratedValue(generator = "generator", strategy = GenerationType.AUTO)
-    @Column(precision = 19, nullable = false, unique = true)
-    public Long commentId;
+	/** COMMENT_ID */
+	@Id
+	@GeneratedValue(generator = "generator", strategy = GenerationType.AUTO)
+	@Column(precision = 19, nullable = false, unique = true)
+	public Long commentId;
 
-    /** COMMENT */
-    @Column(length = 140, nullable = false, unique = false)
-    public String comment;
+	/** COMMENT */
+	@Lob
+	@Column(length = 2147483647, nullable = false, unique = false)
+	public byte[] comment;
 
-    /** COMMENT_DATETIME */
-    @Column(nullable = true, unique = false)
-    public Timestamp commentDatetime;
+	/** COMMENT_DATETIME */
+	@Column(nullable = true, unique = false)
+	public Timestamp commentDatetime;
 
-    /** USER_ID */
-    @Column(precision = 19, nullable = false, unique = false)
-    public Long userId;
+	/** USER_ID */
+	@Column(precision = 19, nullable = false, unique = false)
+	public Long userId;
 
-    /** ROOM_ID */
-    @Column(precision = 19, nullable = false, unique = false)
-    public Long roomId;
+	/** ROOM_ID */
+	@Column(precision = 19, nullable = false, unique = false)
+	public Long roomId;
 
-    /** room関連プロパティ */
-    @ManyToOne
-    @JoinColumn(name = "ROOM_ID", referencedColumnName = "ROOM_ID")
-    public Room room;
+	/** room関連プロパティ */
+	@ManyToOne
+	@JoinColumn(name = "ROOM_ID", referencedColumnName = "ROOM_ID")
+	public Room room;
 
-    /** user関連プロパティ */
-    @ManyToOne
-    @JoinColumn(name = "USER_ID", referencedColumnName = "USER_ID")
-    public User user;
+	/** user関連プロパティ */
+	@ManyToOne
+	@JoinColumn(name = "USER_ID", referencedColumnName = "USER_ID")
+	public User user;
 }
